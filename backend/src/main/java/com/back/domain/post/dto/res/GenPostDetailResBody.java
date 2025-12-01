cef9c25288f904e7d95ba1facee0333870277bf2
ehgk4245 <ehgkrk14@naver.com>
2025-12-01 16:48:51 +0900

    feat: 게시글 생성 옵션 추가

--- backend/src/main/java/com/back/domain/post/dto/res/GenPostDetailResBody.java
+++ backend/src/main/java/com/back/domain/post/dto/res/GenPostDetailResBody.java
@@ -1,10 +1,19 @@
 package com.back.domain.post.dto.res;
 
+import java.util.List;
+
 public record GenPostDetailResBody(
         String title,
         String content,
         Long categoryId,
         Integer fee,
-        Integer deposit
+        Integer deposit,
+        List<PostOption> options
 ) {
+    public record PostOption(
+        String name,
+        Integer fee,
+        Integer deposit
+    ) {
+    }
 }
